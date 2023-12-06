3ffcd46161e46df55bd4eea6843d3bb31ee1b965
Alexander Kraus <a.kraus@glp.de>
2023-12-06 13:24:16 +0100

    Added test and renamed bindings package to not interfer with builds.

---
+++ bindings/Python/sointu_bindings/test_bindings.py
@@ -0,0 +1,13 @@
+from unittest import (
+    TestCase,
+    main,
+)
+from typing import Self
+from sointu import Pattern
+
+class TestBindings(TestCase):
+    def test(self: Self) -> None:
+        pass
+
+if __name__ == '__main__':
+    main()
